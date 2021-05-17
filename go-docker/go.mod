e97162167876261c62cfd8e23115c8fe92e7ec78
fabian.muehlenbein <fabian15121998@googlemail.com>
2021-05-17 18:30:39 +0200

    feat: add small server app which reads data from a static file + full docker deployment

---
+++ go-docker/go.mod
@@ -0,0 +1,8 @@
+module go-docker
+
+go 1.16
+
+require (
+	github.com/afdolriski/golang-docker v0.0.0-20200113125449-123a05bb2f65
+	github.com/gin-gonic/gin v1.7.1
+)
---
+++ go-docker/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/afdolriski/golang-docker/database"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/animal/:name", func(c *gin.Context) {
+		animal, err := database.GetAnimal(c.Param("name"))
+		if err != nil {
+			c.String(404, err.Error())
+			return
+		}
+		c.JSON(200, animal)
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run(":3000")
+	if err != nil {
+		return
+	}
+}
