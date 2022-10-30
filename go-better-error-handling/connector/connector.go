65b7ee31d8f74c17d4314664ebc1c2e1c6e85268
fabian.muehlenbein <fabian15121998@googlemail.com>
2022-10-30 23:13:08 +0100

    Add mongodb to app
    - add basic mongodb connection
    - add docker compose for easy setting up container
    - add method for adding some sample objects to mongodb
    - add simple get handler for retrieving a document from mongodb

---
+++ go-better-error-handling/connector/connector.go
@@ -0,0 +1,78 @@
+package connector
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Trainer struct {
+    Name string
+    Age  int
+    City string
+}
+
+const uri = "mongodb://user:pass@localhost:27017/"
+
+func Connect() (*mongo.Client, error) {
+
+	fmt.Println("Init connection...")
+
+	// Set client options
+	clientOptions := options.Client().ApplyURI(uri)
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	return client, err
+}
+
+func SetupData(client *mongo.Client, ) {
+	collection := client.Database("test").Collection("trainers")
+
+	ash := Trainer{"Ash", 10, "Pallet Town"}
+	misty := Trainer{"Misty", 10, "Cerulean City"}
+	brock := Trainer{"Brock", 15, "Pewter City"}
+
+	trainers := []interface{}{ash, misty, brock}
+
+	insertResult, err := collection.InsertMany(context.TODO(), trainers)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted multiple documents: ", insertResult.InsertedIDs)
+}
+
+func GetTrainer(client *mongo.Client, name string) Trainer {
+	collection := client.Database("test").Collection("trainers")
+
+	filter := bson.D{{Key: "name", Value: name}}
+	var result Trainer
+
+	err := collection.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Found a single document: %+v\n", result)
+
+	return result
+}
--- go-better-error-handling/server.go
+++ go-better-error-handling/server.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"go-better-error-handling/connector"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	client, err := connector.Connect()
+	if err != nil {
+		panic(err)
+	}
+
+	// connector.SetupData(client)
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Wolrd!")
+	})
+	e.GET("/trainer/:name", func(c echo.Context) error {
+		name := c.Param("name")
+		trainer := connector.GetTrainer(client, name)
+
+		return c.JSON(http.StatusOK, trainer)
 	})
 	e.Logger.Fatal(e.Start(":8000"))
 }
